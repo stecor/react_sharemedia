fd18c30eb4b7c41cb7586ca4bf792e948cc53cdd
Stefano Corra <stefano.corra@hotmail.com>
2021-12-29 19:45:31 -0500

    login screen

--- sharemedia_frontend/src/components/Login.jsx
+++ sharemedia_frontend/src/components/Login.jsx
@@ -5,9 +5,30 @@ import { FcGoogle } from 'react-icons/fc'
 import shareVideo from '../assets/share.mp4'
 import logo from '../assets/logowhite.png'
 
-
+import { client } from '../client'
 
 const Login = () => {
+
+  const navigate = useNavigate()
+
+  const responseGoogle=(response) => {
+    localStorage.setItem('user', JSON.stringify(response.profileObj))
+
+    const {name, googleId, imageUrl} = response.profileObj
+
+    const doc={
+      _id: googleId,
+      _type: 'user',
+      userName: name,
+      image: imageUrl
+
+    }
+
+    client.createIfNotExists(doc)
+    .then(() => {
+      navigate('/',{replace: true})
+    })
+  }
     return (
         <div className='flex justify-start items-center flex-col h-screen'>
             <div className='relative w-full h-full'>
@@ -26,17 +47,23 @@ const Login = () => {
                   </div>
                   <div className='shadow-2xl'>
                   <GoogleLogin
-                      clientId=''
+                      clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
                       render={(renderProps) => (
                         <button
                             type='button'
-                            className='bg-mainColor'
+                            className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'
+                          onClick={renderProps.onClick}
+                          disabled={renderProps.disabled}
                           >
                               <FcGoogle
-                                className='bg-mainColor'
+                                className='mr-4'
                               />
+                              Sign in with Google
                         </button>
                       )}
+                      onSuccess={responseGoogle}
+                      onFailure={responseGoogle}
+                      cookiePolicy='single_host_origin'
                   />
 
                   </div>
