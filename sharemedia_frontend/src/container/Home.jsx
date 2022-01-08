6d745e0141d053644a5fbadcc3931914447d89f7
Stefano Corra <stefano.corra@hotmail.com>
2022-01-07 22:27:39 -0500

    menu screen

--- sharemedia_frontend/src/container/Home.jsx
+++ sharemedia_frontend/src/container/Home.jsx
@@ -12,12 +12,13 @@ import logo from '../assets/logo.png'
 const Home = () => {
 
 const [toggleSideBar, setToggleSideBar] = useState(false)
-
 const [user, setUser] = useState(null)
+const scrollRef = (null)
 
 const userInfo = localStorage.getItem('user') !== 'undefined' ?
                       JSON.parse(localStorage.getItem('user')) :
                       localStorage.clear()
+
 
 useEffect(() => {
    const query = userQuery(userInfo?.googleId)
@@ -28,12 +29,17 @@ useEffect(() => {
    })
 }, [])
 
+// useEffect(() => {
+//  scrollRef.current.scrollTo(0,0)
+// })
+
     return (
         <div className='flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out'>
           <div className= 'hidden md:flex h-screen flex-initial'>
           <Sidebar user={user && user} />
           </div>
           <div className='flex md:hidden flex-row'>
+            <div className="p-2 w-full flex flex-row justify-between items-center shadow-md">
             <HiMenu fontSize={40} className='cursor-pointer' onClick={ () => setToggleSideBar(true) }/>
             <Link to='/'>
               <img src={logo} alt="logo" className="w-28"/>
@@ -47,9 +53,16 @@ useEffect(() => {
               <div className="absolute w-full flex justify-end items-center p-2">
                 <AiFillCloseCircle fontSize={30} className="cursor-pointer" onClick={() => setToggleSideBar(false)}/>
               </div>
-            <Sidebar user={user && user} closeToggle={setToggleSideBar}/>
+            <Sidebar closeToggle={setToggleSideBar} user={user && user} />
             </div>
           )}
+          </div>
+          <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+            <Routes>
+               <Route path="/user-profile/:userId" element={<UserProfile />} />
+               <Route path="/*" element={<Pins user={user && user} />} />
+             </Routes>
+          </div>
         </div>
     )
 }
